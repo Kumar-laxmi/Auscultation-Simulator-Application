93649d00e16d151b15579d5d136a3a4cee93e8e1
Kumar-laxmi <laxmikantk2002@gmail.com>
2024-02-06 17:01:05 +0530

    Last commit of work

--- sound.py
+++ sound.py
@@ -1,27 +1,81 @@
-import sounddevice as sd
-import soundfile as sf
-import threading
-from threading import Thread
+import dash
+from urllib import request
+from dash import dcc, html
+from dash.dependencies import Input, Output, State
+from pydub import AudioSegment
+from django_plotly_dash import DjangoDash
+import numpy as np
+import pandas as pd
 import time
+import sqlite3
 
-def audioFunc1():
-    audio1 = "app/static/audio/heart/normal_heart/A/combined_audio.wav"
-    data1, fs1 = sf.read(audio1, dtype='float32')
-    bpm = 60
-    delay=60/bpm
-    while True:
-        sd.play(data1, fs1, device=2)   #speakers
-        time.sleep(delay)
+# Create Dash app
+app = DjangoDash('hbrDash')
+con = sqlite3.connect("db.sqlite3")
+cur = con.cursor()
+df = pd.read_sql_query("SELECT * FROM app_heartaudio", con)
 
-def audioFunc2():
-    audio2 = "app/static/audio/heart/opening_snap/A/2.wav"
-    data2, fs2 = sf.read(audio2, dtype='float32')
-    bpm = 60
-    delay=60/bpm
-    while True:
-        sd.play(data2, fs2, device=3)  #headset
-        time.sleep(delay)
+audio_path = df.loc[(df['sound_name'] == 'normal_heart') & (df['sound_type'] == 'T'), 'audio_file_path'].values[0]
+audio = AudioSegment.from_file(audio_path)
+audio_data = np.array(audio.get_array_of_samples())
+audio_duration = len(audio_data) / audio.frame_rate
+subsampling_factor = 1
+audio_data = audio_data[::subsampling_factor]
 
-if __name__ == '__main__':
-    Thread(target = audioFunc1).start()
-    Thread(target = audioFunc2).start()
+target_duration = 0.877
+num_repeats = 1
+audio_data = np.tile(audio_data, num_repeats)
+sample_rate = 44100
+duration = len(audio_data) / sample_rate
+
+heartRate = 60
+
+# Layout of the app
+app.layout = html.Div([
+    dcc.Graph(id='animated-audio-chart', style={'height': '95vh'}, config={'responsive': True}),
+    dcc.Store(id='audio-data-store', data={'audio_data': audio_data.tolist(), 'audio_duration': audio_duration}),
+    dcc.Store(id='interval-store', data=time.time()),  # Store the start time and set default heart rate to 60
+    dcc.Interval(id='interval-component', interval=25, n_intervals=0) # Interval in milliseconds
+])
+
+# Clientside callback to update the graph
+app.clientside_callback(
+    """
+    function(n, audioData, startTime) {
+        // Get the audio data and duration from the stored data
+        var audioArray = audioData['audio_data'];
+        var audioDuration = audioData['audio_duration'];
+        var heartRate = 60
+
+        // Calculate the time passed since the start
+        var currentTime = new Date().getTime() / 1000;  // Convert milliseconds to seconds
+        var elapsedTime = currentTime - startTime;
+
+        // Calculate the position of the vertical line
+        var timePerBeat = 60 / heartRate;
+        var linePosition = Math.floor((elapsedTime % timePerBeat) * audioArray.length / timePerBeat);
+
+        // Create the figure
+        var figure = {
+            data: [
+                {y: audioArray, type: 'line', name: 'HBR Signal', line: {color: 'green'}},
+                {x: [linePosition, linePosition], y: [Math.min.apply(null, audioArray), Math.max.apply(null, audioArray)], mode: 'lines', line: {color: 'black', width: 10}}
+            ],
+            layout: {
+                title: 'Normal Heart sound (Mitral valve)',
+                showlegend: false,
+                paper_bgcolor: 'black',  // Set background color to black
+                plot_bgcolor: 'black'    // Set plot background color to black
+            }
+        };
+        
+        // Return the updated figure, startTime, and heartRate
+        return [figure, startTime];
+    }
+    """,
+    Output('animated-audio-chart', 'figure'),
+    Output('interval-store', 'data'),
+    Input('interval-component', 'n_intervals'),
+    State('audio-data-store', 'data'),
+    State('interval-store', 'data')
+)
